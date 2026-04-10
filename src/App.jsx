f8b28fc703c81b6732be37919e291cd4341879a0
Luis Rios Alvarado <luismanuelrios123@gmail.com>
2026-04-10 08:43:12 -0400

    Heder code created

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+
 import './App.css'
+
+import Header from './components/Header'
 
 function App() {
 
   return (
     <>
-      
+      <Header />
     </>
   )
 }
