26f580e1ceb7cfb27d7779b7e19415b32fc05091
Luis Rios Alvarado <luismanuelrios123@gmail.com>
2026-04-10 12:11:47 -0400

    I've added global themes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
-import { useState } from 'react'
+
+import { useState } from 'react';
+import { ThemeProvider, CssBaseline, Button } from '@mui/material';
+import { lightTheme, darkTheme } from './style/theme';
 
 import './App.css'
 
 import Header from './components/Header'
 
+
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const handleThemeToggle = () => {
+    setIsDarkMode((prev) => !prev);
+  };
 
   return (
-    <>
+    <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
+      <CssBaseline />
       <Header />
-    </>
-  )
+      <div style={{ padding: '1rem' }}>
+        <Button variant="contained" onClick={handleThemeToggle}>
+          Switch to {isDarkMode ? 'Light' : 'Dark'} Mode
+        </Button>
+      </div>
+    </ThemeProvider>
+  );
 }
 
 export default App
