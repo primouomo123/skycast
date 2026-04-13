ef8ef71aeaab3bc79bf2c3c03aa51f3af987d7e1
Luis Rios Alvarado <luismanuelrios123@gmail.com>
2026-04-12 21:57:00 -0400

    Testing button deleted

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import { useState } from 'react';
-import { ThemeProvider, CssBaseline, Button } from '@mui/material';
+import { ThemeProvider, CssBaseline } from '@mui/material';
 import { lightTheme, darkTheme } from './style/theme';
 
 import './App.css'
@@ -18,12 +18,7 @@ function App() {
   return (
     <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
       <CssBaseline />
-      <Header />
-      <div style={{ padding: '1rem' }}>
-        <Button variant="contained" onClick={handleThemeToggle}>
-          Switch to {isDarkMode ? 'Light' : 'Dark'} Mode
-        </Button>
-      </div>
+      <Header onDarkModeToggle={handleThemeToggle} isDarkMode={isDarkMode} />
     </ThemeProvider>
   );
 }
